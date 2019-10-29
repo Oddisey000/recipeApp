2770b8a72f1f90d6a11b68fc5c79ae606f899e12
Oddisey000 <oddisey000@gmail.com>
2019-10-29 20:22:55 +0200

    Create structure of document

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'A simple recipe description', 'https://cdn.pixabay.com/photo/2016/04/09/09/22/pizza-1317699_1280.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
