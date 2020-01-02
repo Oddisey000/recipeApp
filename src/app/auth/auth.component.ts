161dfdac834ad2e9c3afea150f0be7902958fc5d
Oddisey000 <oddisey000@gmail.com>
2020-01-02 12:59:32 +0200

    Outsource components from app module, add modal error window

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { AlertComponent } from '../alert/alert.component';
+import { PlaceholderDirective } from '../placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode: boolean = true;
   isLoading: boolean = false;
   isError: string = null;
 
+  private closeModal: Subscription;
+  
+  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+
   constructor(
     private authService: AuthService,
-    private router: Router) { }
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  private showErrorAlert(message: string) {
+    const alertCompFactory = this.componentFactoryResolver
+      .resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(alertCompFactory);
+    componentRef.instance.message = message;
+    this.closeModal = componentRef.instance.close.subscribe(() => {
+      this.closeModal.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+  }
 
   ngOnInit() {
   }
@@ -47,11 +69,20 @@ export class AuthComponent implements OnInit {
       },
       (error: any) => {
         this.isError = error;
+        this.showErrorAlert(error);
         this.isLoading = false;
       }
     );    
     
     form.reset();
+  }
+
+  onHandleError() {
+    this.isError = null;
+  }
+
+  ngOnDestroy() {
+    if (this.closeModal) this.closeModal.unsubscribe();
   }
 
 }
